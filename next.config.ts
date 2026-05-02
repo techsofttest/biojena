b98bc815cb6dfbcb5cebae38286209034753e5cc
techsofttest <techsofttest@gmail.com>
2026-05-02 11:51:51 +0530

    feat: latest updates and refinements

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
-export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Forces Next.js to build a static HTML site
+  images: {
+    unoptimized: true, // THIS IS THE FIX FOR BIGROCK
+  },
+}
+
+module.exports = nextConfig
